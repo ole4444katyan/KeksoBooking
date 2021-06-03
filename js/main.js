77731cf796f7c16e6b78c3e4e544b9f1daaaa71e
ole4444katyan <ole44katyan.work@gmail.com>
2021-06-03 13:05:16 +0300

    Исправила замечания

--- js/main.js
+++ js/main.js
@@ -1,24 +1,26 @@
-function getRandomFromRange (min, max) {
+const getRandomFromRange = (min, max) => {
   if ( min < 0 || min > max) {
-    throw new Error;
+    throw new Error ('Минимальное значение отрицательное или больше максимального');
   }
+
   min = Math.floor(min);
   max = Math.ceil(max);
   return Math.round(Math.random() * (max - min + 1)) + min;
-}
+};
 
-function getRandomFloatFromRange (min, max, point) {
+const getRandomFloatFromRange = (min, max, point) => {
   if ( min < 0 || min > max) {
-    throw new Error;
+    throw new Error ('Минимальное значение отрицательное или больше максимального');
   }
+
   point = Math.pow(10, point);
 
   min = min*point;
   max = max*point;
   return Math.round((Math.random() * (max - min + 1)) + min)/point;
-}
+};
 
-console.log(getRandomFromRange(3.2, 4));
+getRandomFromRange(3.2, 4);
 getRandomFloatFromRange(3.7, 4, 1);
 
 //первое решение взято отсюда https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
