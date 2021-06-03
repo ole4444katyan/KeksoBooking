d71364e214d64840c4086dd3493431cd19f93f94
ole4444katyan <ole44katyan.work@gmail.com>
2021-06-03 13:16:41 +0300

    Поняла почему используется ceil, floor, floor и исправила первую функцию

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ const getRandomFromRange = (min, max) => {
     throw new Error ('Минимальное значение отрицательное или больше максимального');
   }
 
-  min = Math.floor(min);
-  max = Math.ceil(max);
-  return Math.round(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 const getRandomFloatFromRange = (min, max, point) => {
@@ -15,12 +15,12 @@ const getRandomFloatFromRange = (min, max, point) => {
 
   point = Math.pow(10, point);
 
-  min = min*point;
-  max = max*point;
+  min *= point;
+  max *= point;
   return Math.round((Math.random() * (max - min + 1)) + min)/point;
 };
 
 getRandomFromRange(3.2, 4);
-getRandomFloatFromRange(3.7, 4, 1);
+getRandomFloatFromRange(3.7, 4, 2);
 
 //первое решение взято отсюда https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
