2b4a457d81a91af354ba84a786bc52af99136c01
ole4444katyan <ole44katyan.work@gmail.com>
2021-06-03 12:30:18 +0300

    Добавила вторую вспомогательную функцию

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 function getRandomFromRange (min, max) {
-  if ( min > 0 && min < max) {
-    min = Math.round(min);
-    max = Math.round(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  if ( min < 0 || min > max) {
+    throw new Error;
   }
-  throw new Error;
+  min = Math.round(min);
+  max = Math.round(max);
+  return Math.round(Math.random() * (max - min + 1)) + min;
 }
 
-getRandomFromRange();
+function getRandomFloatFromRange (min, max, point) {
+  if ( min < 0 || min > max) {
+    throw new Error;
+  }
+  point = Math.pow(10, point);
 
-//решение взято отсюда https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  min = min*point;
+  max = max*point;
+  return Math.round((Math.random() * (max - min + 1)) + min)/point;
+}
+
+getRandomFromRange(3, 5);
+getRandomFloatFromRange(3.7, 4, 1);
+
+//первое решение взято отсюда https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
